a754d47c8673112b5229cb7b9272fe3e47c1b315
Jacob Montgomery <jm15969@bristol.ac.uk>
2017-03-10 14:44:13 +0000

    added svc to hilevel.c in CW

--- CW2/question/kernel/hilevel.c
+++ CW2/question/kernel/hilevel.c
@@ -8,6 +8,36 @@ void hilevel_handler_irq() {
   return;
 }
 
-void hilevel_handler_svc() {
+void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) {
+  /* Based on the identified encoded as an immediate operand in the
+   * instruction,
+   *
+   * - read  the arguments from preserved usr mode registers,
+   * - perform whatever is appropriate for this system call,
+   * - write any return value back to preserved usr mode registers.
+   */
+
+switch( id ) {
+    case 0x00 : { // 0x00 => yield()
+      scheduler( ctx );
+      break;
+    }
+    case 0x01 : { // 0x01 => write( fd, x, n ) copy this block into CW2
+      int   fd = ( int   )( ctx->gpr[ 0 ] );
+      char*  x = ( char* )( ctx->gpr[ 1 ] );
+      int    n = ( int   )( ctx->gpr[ 2 ] );
+
+      for( int i = 0; i < n; i++ ) {
+        PL011_putc( UART0, *x++, true );
+      }
+
+      ctx->gpr[ 0 ] = n;
+      break;
+    }
+    default   : { // 0x?? => unknown/unsupported
+      break;
+    }
+  }
+
   return;
 }
