3aca77438a220ede625ecc56c0e5d3d28cb2a7b4
Jacob Montgomery <jm15969@bristol.ac.uk>
2017-03-11 10:42:18 +0000

    CW2 lolevel.s

--- CW2/question/kernel/lolevel.s
+++ CW2/question/kernel/lolevel.s
@@ -4,8 +4,8 @@
  */
 
 .global lolevel_handler_rst
-.global lolevel_handler_irq
 .global lolevel_handler_svc
+.global lolevel_handler_irq
 
 lolevel_handler_rst: bl    int_init                @ initialise interrupt vector table
 
@@ -14,21 +14,50 @@ lolevel_handler_rst: bl    int_init                @ initialise interrupt vector
                      msr   cpsr, #0xD3             @ enter SVC mode with IRQ and FIQ interrupts disabled
                      ldr   sp, =tos_svc            @ initialise SVC mode stack
 
+                     sub   sp, sp, #68             @ initialise dummy context
+
+                     mov   r0, sp                  @ set    high-level C function arg. = SP
                      bl    hilevel_handler_rst     @ invoke high-level C function
-                     b     .                       @ halt
+
+                     ldmia sp!, { r0, lr }         @ load   USR mode PC and CPSR
+                     msr   spsr, r0                @ set    USR mode        CPSR
+                     ldmia sp, { r0-r12, sp, lr }^ @ load   USR mode registers
+                     add   sp, sp, #60             @ update SVC mode SP
+                     movs  pc, lr                  @ return from interrupt
 
 lolevel_handler_irq: sub   lr, lr, #4              @ correct return address
-                     stmfd sp!, { r0-r3, ip, lr }  @ save    caller-save registers
+                     sub   sp, sp, #60             @ update SVC mode stack
+                     stmia sp, { r0-r12, sp, lr }^ @ store  USR registers
+                     mrs   r0, spsr                @ get    USR        CPSR
+                     stmdb sp!, { r0, lr }         @ store  USR PC and CPS
+
+                     mov   r0, sp                  @ set    high-level C function arg. = SP
 
                      bl    hilevel_handler_irq     @ invoke high-level C function
 
-                     ldmfd sp!, { r0-r3, ip, lr }  @ restore caller-save registers
+                     ldr   r1, [ lr, #-4 ]         @ load                     svc instruction
+                     bic   r1, r1, #0xFF000000     @ set    high-level C function arg. = svc immediate
+                     bl    hilevel_handler_irq     @ invoke high-level C function
+
+                     ldmia sp!, { r0, lr }         @ load USR mode PS and CSPR
+                     msr   spsr, r0                @ set    USR mode        CPSR
+                     ldmia sp, { r0-r12, sp, lr }^ @ load   USR mode registers
+                     add   sp, sp, #60             @ update SVC mode SP
                      movs  pc, lr                  @ return from interrupt
 
 lolevel_handler_svc: sub   lr, lr, #0              @ correct return address
-                     stmfd sp!, { r0-r3, ip, lr }  @ save    caller-save registers
+                     sub   sp, sp, #60             @ update SVC mode stack
+                     stmia sp, { r0-r12, sp, lr }^ @ store  USR registers
+                     mrs   r0, spsr                @ get    USR        CPSR
+                     stmdb sp!, { r0, lr }         @ store  USR PC and CPSR
 
+                     mov   r0, sp                  @ set    high-level C function arg. = SP
+                     ldr   r1, [ lr, #-4 ]         @ load                     svc instruction
+                     bic   r1, r1, #0xFF000000     @ set    high-level C function arg. = svc immediate
                      bl    hilevel_handler_svc     @ invoke high-level C function
 
-                     ldmfd sp!, { r0-r3, ip, lr }  @ restore caller-save registers
-                     movs  pc, lr                  @ return from interrupt 
+                     ldmia sp!, { r0, lr }         @ load   USR mode PC and CPSR
+                     msr   spsr, r0                @ set    USR mode        CPSR
+                     ldmia sp, { r0-r12, sp, lr }^ @ load   USR mode registers
+                     add   sp, sp, #60             @ update SVC mode SP
+                     movs  pc, lr                  @ return from interrupt
