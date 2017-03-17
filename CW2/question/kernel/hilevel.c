bf8a70360fe139b35334e8bbb67f76d26a429ccc
Jacob Montgomery <jm15969@bristol.ac.uk>
2017-03-17 14:14:55 +0000

    CW2 hilevel.c

--- CW2/question/kernel/hilevel.c
+++ CW2/question/kernel/hilevel.c
@@ -62,8 +62,8 @@ void hilevel_handler_rst( ctx_t* ctx              ) {
    pcb[ 2 ].ctx.sp   = ( uint32_t )( &tos_P5  );
 
    current = &pcb[ 0 ]; memcpy( ctx, &current->ctx, sizeof( ctx_t ) );
-   current = &pcb[ 1 ]; memcpy( ctx, &current->ctx, sizeof( ctx_t ) );
-   current = &pcb[ 2 ]; memcpy( ctx, &current->ctx, sizeof( ctx_t ) );
+   //current = &pcb[ 1 ]; memcpy( ctx, &current->ctx, sizeof( ctx_t ) );
+   //current = &pcb[ 2 ]; memcpy( ctx, &current->ctx, sizeof( ctx_t ) );
 
   TIMER0->Timer1Load  = 0x00100000; // select period = 2^20 ticks ~= 1 sec
   TIMER0->Timer1Ctrl  = 0x00000002; // select 32-bit   timer
