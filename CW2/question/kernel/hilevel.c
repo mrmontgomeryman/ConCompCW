bd22a83e39e15b9b9383b8c1d6a8b02303d22d9e
Jacob Montgomery <jm15969@bristol.ac.uk>
2017-03-17 14:59:43 +0000

    CW2 hilevel.c

--- CW2/question/kernel/hilevel.c
+++ CW2/question/kernel/hilevel.c
@@ -1,6 +1,6 @@
 #include "hilevel.h"
 
-pcb_t pcb[ 2 ], *current = NULL;
+//pcb_t pcb[ 2 ], *current = NULL;
 
 void scheduler( ctx_t* ctx ) {
   if      ( current == &pcb[ 0 ] ) {
@@ -61,7 +61,7 @@ void hilevel_handler_rst( ctx_t* ctx              ) {
    pcb[ 2 ].ctx.pc   = ( uint32_t )( &main_P5 );
    pcb[ 2 ].ctx.sp   = ( uint32_t )( &tos_P5  );
 
-   current = &pcb[ 0 ]; memcpy( ctx, &current->ctx, sizeof( ctx_t ) );
+   //current = &pcb[ 0 ]; memcpy( ctx, &current->ctx, sizeof( ctx_t ) );
 
   TIMER0->Timer1Load  = 0x00100000; // select period = 2^20 ticks ~= 1 sec
   TIMER0->Timer1Ctrl  = 0x00000002; // select 32-bit   timer
