772690b226e076dee7d6b8eb566a5b7881cbe1c7
Jacob Montgomery <jm15969@bristol.ac.uk>
2017-03-21 10:50:04 +0000

    cw2 hilevel and lolevel

--- CW2/question/kernel/int.s
+++ CW2/question/kernel/int.s
@@ -1,13 +1,13 @@
 /* The following captures the interrupt vector table, plus a function
- * to copy it into place (which is called on reset): note that 
- * 
+ * to copy it into place (which is called on reset): note that
+ *
  * - for interrupts we don't handle an infinite loop is realised (to
  *   to approximate halting the processor), and
  * - we copy the table itself, *plus* the associated addresses stored
- *   as static data: this preserves the relative offset between each 
+ *   as static data: this preserves the relative offset between each
  *   ldr instruction and wherever it loads from.
  */
-	
+
 int_data:            ldr   pc, int_addr_rst        @ reset                 vector -> SVC mode
                      b     .                       @ undefined instruction vector -> UND mode
                      ldr   pc, int_addr_svc        @ supervisor call       vector -> SVC mode
@@ -20,9 +20,9 @@ int_data:            ldr   pc, int_addr_rst        @ reset                 vecto
 int_addr_rst:        .word lolevel_handler_rst
 int_addr_svc:        .word lolevel_handler_svc
 int_addr_irq:        .word lolevel_handler_irq
-	
+
 .global int_init
-	
+
 int_init:            mov   r0, #0                  @ set destination address
                      ldr   r1, =int_data           @ set source      address = start of     data
                      ldr   r2, =int_init           @ set source      limit   = start of function
@@ -30,9 +30,9 @@ int_init:            mov   r0, #0                  @ set destination address
 l0:                  ldr   r3, [ r1 ], #4          @ load  word, inc. source      address
                      str   r3, [ r0 ], #4          @ store word, inc. destination address
 
-                     cmp   r1, r2                  
+                     cmp   r1, r2
                      bne   l0                      @ loop if address != limit
-               
+
                      mov   pc, lr                  @ return
 
 /* These function enable and disable IRQ and FIQ interrupts, toggling
@@ -43,27 +43,27 @@ l0:                  ldr   r3, [ r1 ], #4          @ load  word, inc. source    
 .global int_unable_irq
 .global int_enable_fiq
 .global int_unable_fiq
-	
+
 int_enable_irq:      mrs   r0,   cpsr              @ get USR mode CPSR
                      bic   r0, r0, #0x80           @  enable IRQ interrupts
                      msr   cpsr_c, r0              @ set USR mode CPSR
-        
+
                      mov   pc, lr                  @ return
 
 int_unable_irq:      mrs   r0,   cpsr              @ get USR mode CPSR
                      orr   r0, r0, #0x80           @ disable IRQ interrupts
                      msr   cpsr_c, r0              @ set USR mode CPSR
-        
+
                      mov   pc, lr                  @ return
 
 int_enable_fiq:      mrs   r0,   cpsr              @ get USR mode CPSR
                      bic   r0, r0, #0x40           @  enable FIQ interrupts
                      msr   cpsr_c, r0              @ set USR mode CPSR
-        
+
                      mov   pc, lr                  @ return
 
 int_unable_fiq:      mrs   r0,   cpsr              @ get USR mode CPSR
                      orr   r0, r0, #0x40           @ disable FIQ interrupts
                      msr   cpsr_c, r0              @ set USR mode CPSR
-        
+
                      mov   pc, lr                  @ return
