3f295f07103da6a0255f49e153c1dde2349e62bf
Jacob Montgomery <jm15969@bristol.ac.uk>
2017-03-24 15:11:07 +0000

    CW2 hilevel.c

--- CW2/question/user/P3.c
+++ CW2/question/user/P3.c
@@ -15,16 +15,17 @@ int is_prime( uint32_t x ) {
 }
 
 void main_P3() {
-  for( int i = 0; i < 50; i++ ) {
+  while (1) {
+  //for( int i = 0; i < 50; i++ ) {
     write( STDOUT_FILENO, "P3", 2 );
 
     uint32_t lo = 1 <<  8;
     uint32_t hi = 1 << 16;
 
     for( uint32_t x = lo; x < hi; x++ ) {
-      int r = is_prime( x ); 
+      int r = is_prime( x );
     }
   }
 
-  exit( EXIT_SUCCESS );
+  //exit( EXIT_SUCCESS );
 }
