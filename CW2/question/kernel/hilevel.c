775acff79180e39a64ea9fd23cb240ac6d8d1dfe
Jacob Montgomery <jm15969@bristol.ac.uk>
2017-03-29 16:28:04 +0100

    hilevel.c CW2

--- CW2/question/kernel/hilevel.c
+++ CW2/question/kernel/hilevel.c
@@ -8,7 +8,7 @@ void scheduler( ctx_t* ctx ) {
     //memcpy( ctx, &pcb[ 1 ].ctx, sizeof( ctx_t ) ); // restore  P_4
     //memcpy( ctx, &pcb[ 2 ].ctx, sizeof( ctx_t ) ); // restore  P_2
 
-    if ((pid=fork())==0){
+    if ((pid_t pid=fork())==0){
       memcpy( ctx, &pcb[ 0 ].ctx, sizeof( ctx_t ) ); // restore  P_3
     }
       else {
